7395198af604784fb53bb8d753baa3ea6ed5b86c
meyraanindita9-lgtm <meyraanindita9@gmail.com>
2026-01-12 09:59:18 +0700

    Tambah input nilai & nama

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿int nilai;
+﻿using System.Runtime.InteropServices.Marshalling;
+
+int nilai;
 string name;
+
 Console.Write("Aplikasi Nilai Siswa: ");
+
+Console.Write("Input Nama: ");
+name = Console.ReadLine();
+
+Console.Write("Input Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
+ 
